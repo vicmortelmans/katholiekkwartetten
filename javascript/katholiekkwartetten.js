e306f4da2b52e528d6fa69291c8154fd9ecf8900
Vic Mortelmans <vicmortelmans@gmail.com>
2013-09-11 13:17:17 +0200

    finishing for first release

--- javascript/katholiekkwartetten.js
+++ javascript/katholiekkwartetten.js
@@ -22,3 +22,15 @@ $(document).ready(function(e) {
         }
     });
 });
+/*
+ following section fixes flickering of page transitions on Android
+ and must be imported between jQuery and jQuery Mobile
+ */
+$(document).bind("mobileinit", function()
+{
+   if (navigator.userAgent.indexOf("Android") != -1)
+   {
+     $.mobile.defaultPageTransition = 'none';
+     $.mobile.defaultDialogTransition = 'none';
+   }
+});
